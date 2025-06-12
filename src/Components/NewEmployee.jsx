d2521f93be0f7bbb52aa716031a5ae850ebd5310
Iqra Ali <ali.iqra2005@gmail.com>
2025-06-12 21:33:38 +0530

    your commit message

--- src/Components/NewEmployee.jsx
+++ src/Components/NewEmployee.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addEmployees, getEmployeebyId, updateEmployee } from '../Services/employeeService';
+import { addEmployees, getEmployeebyId, updateEmployee } from '../Services/EmployeeService';
 import { useNavigate, useParams } from 'react-router-dom';
 
 function NewEmployee() {
