2e64b310b8c45aaef59db5e9428c6a64974a40f7
Iqra Ali <ali.iqra2005@gmail.com>
2025-06-12 21:25:48 +0530

    your commit message

--- src/Services/EmployeeService.jsx
+++ src/Services/EmployeeService.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/employees';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getEmployees = () => axios.get(API_BASE_URL); // client code for get request
 
 export const addEmployees = (employee) => axios.post(API_BASE_URL, employee);// client code for post request
 
-export const getEmployeebyId = (employeeId) => axios.get(API_BASE_URL + '/' + employeeId);// Get employee details by ID (for editing)
+export const getEmployeebyId = (employeeId) => axios.get(`${API_BASE_URL}/${employeeId}`);// Get employee details by ID (for editing)
 
-export const updateEmployee = (employeeId, employee) => axios.put(API_BASE_URL + '/' + employeeId, employee);// Update employee details
+export const updateEmployee = (employeeId, employee) => axios.put(`${API_BASE_URL}/${employeeId}`, employee);// Update employee details
 
-export const deleteEmployee = (employeeId) => axios.delete(API_BASE_URL + '/' + employeeId);// Delete employee by ID
+export const deleteEmployee = (employeeId) => axios.delete(`${API_BASE_URL}/${employeeId}`);// Delete employee by ID
